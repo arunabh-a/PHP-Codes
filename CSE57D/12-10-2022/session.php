9dab17b0deaad629d7a038a69bd15d536e3bde9d
Arunabh <arunabh.nd@gmail.com>
2022-10-12 16:07:45 +0530

    Semester 5 Practicals

--- CSE57D/12-10-2022/session.php
+++ CSE57D/12-10-2022/session.php
@@ -1,4 +1,15 @@
 <?php
+    session_start();
+?>
+<?php
+    $_SESSION["Name"] = "Arunabh";
+    $_SESSION["Class"] = "JK-202";
+    
+?>
+
+<?php
     echo "The Student Name is ". $_SESSION["Name"]. "<br>";
     echo "The Class of the Student is ". $_SESSION["Class"]. "<br>";
 ?>
+
+
--- CSE57D/12-10-2022/sessvar.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-?>
-<?php
-    $_SESSION["Name"] = "Arunabh";
-    $_SESSION["Class"] = "JK-202";
-?>
